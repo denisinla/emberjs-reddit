1ac9ee59351a7787e3fd45e34867d3639e6f792b
Denis Kabistan <dkabistan@gmail.com>
2014-08-13 22:53:05 -0700

    Initial import of codebase.

---
+++ app/models/store.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+var cache = {};
+
+export default Ember.Object.extend({
+  find: function(name, id){
+    // subreddit
+    if(cache[name] && cache[name][id]){
+      return cache[name][id];
+    }
+
+    var adapter = this.container.lookup('adapter:' + name);
+    return adapter.find(name, id).then(function(record){
+      cache[name] = cache[name] || {};
+      cache[name][id] = record;
+      return record;
+    });
+  }
+});
