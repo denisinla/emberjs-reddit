f1a783e3705d252bfbd073ded984ce0582a58a6e
Denis Kabistan <dkabistan@gmail.com>
2014-08-14 01:02:16 -0700

    Update: Changed models being shown for reddit routes.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return ['aww','pugs','corgi'];
+    return ['emberjs','javascript','backbonejs'];
   }
 });
